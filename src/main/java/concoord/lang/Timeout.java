0e4b95211f6688aed88cd1cc0c812c0d70ff9b11
davide-maestroni <davide.maestroni@gmail.com>
2021-03-30 23:38:01 +0200

    Timeout

--- src/main/java/concoord/lang/Timeout.java
+++ src/main/java/concoord/lang/Timeout.java
@@ -18,12 +18,271 @@ package concoord.lang;
 import concoord.concurrent.Awaitable;
 import concoord.concurrent.Scheduler;
 import concoord.concurrent.Task;
+import concoord.lang.BaseAwaitable.BaseFlowControl;
+import concoord.lang.BaseAwaitable.ExecutionControl;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.TimeUnit;
 import org.jetbrains.annotations.NotNull;
 
 public class Timeout implements Task<Long> {
 
+  private static final Object timerMutex = new Object();
+  private static Timer timer;
+
+  private final ControlFactory controlFactory;
+
+  public Timeout(@NotNull Date delay) {
+    this(System.currentTimeMillis() - delay.getTime(), TimeUnit.MILLISECONDS);
+  }
+
+  public Timeout(long delay, @NotNull TimeUnit timeUnit) {
+    controlFactory = new DelayControlFactory(timeUnit.toMillis(delay));
+  }
+
+  public Timeout(@NotNull Date delay, long period, @NotNull TimeUnit timeUnit) {
+    this(System.currentTimeMillis() - delay.getTime(), timeUnit.toMillis(period),
+        TimeUnit.MILLISECONDS);
+  }
+
+  public Timeout(long delay, long period, @NotNull TimeUnit timeUnit) {
+    this(delay, timeUnit, period, timeUnit);
+  }
+
+  public Timeout(long delay, @NotNull TimeUnit delayUnit, long period,
+      @NotNull TimeUnit periodUnit) {
+    controlFactory = new PeriodControlFactory(
+        delayUnit.toMillis(delay),
+        periodUnit.toMillis(period)
+    );
+  }
+
+  @NotNull
+  private static Timer timer() {
+    synchronized (timerMutex) {
+      if (timer == null) {
+        timer = new Timer("concoord-timeout-timer");
+      }
+    }
+    return timer;
+  }
+
   @NotNull
   public Awaitable<Long> on(@NotNull Scheduler scheduler) {
-    return null;
+    return new BaseAwaitable<Long>(scheduler, controlFactory.create(scheduler));
+  }
+
+  private interface ControlFactory {
+
+    @NotNull
+    ExecutionControl<Long> create(@NotNull Scheduler scheduler);
+  }
+
+  private static class DelayControlFactory implements ControlFactory {
+
+    private final long delayMillis;
+
+    private DelayControlFactory(long delayMillis) {
+      this.delayMillis = delayMillis;
+    }
+
+    @NotNull
+    public ExecutionControl<Long> create(@NotNull Scheduler scheduler) {
+      return new DelayControl(scheduler, delayMillis);
+    }
+  }
+
+  private static class DelayControl extends TimerTask implements ExecutionControl<Long> {
+
+    private final StartState startState = new StartState();
+    private final Scheduler scheduler;
+    private final long delayMillis;
+    private ExecutionControl<Long> controlState = startState;
+    private BaseFlowControl<Long> flowControl;
+    private Long message;
+
+    private DelayControl(@NotNull Scheduler scheduler, long delayMillis) {
+      this.scheduler = scheduler;
+      this.delayMillis = delayMillis;
+    }
+
+    public boolean executeBlock(@NotNull BaseFlowControl<Long> flowControl) throws Exception {
+      this.flowControl = flowControl;
+      return controlState.executeBlock(flowControl);
+    }
+
+    public void cancelExecution() throws Exception {
+      controlState.cancelExecution();
+    }
+
+    public void abortExecution(@NotNull Throwable error) throws Exception {
+      controlState.abortExecution(error);
+    }
+
+    public void run() {
+      scheduler.scheduleLow(new MessageCommand());
+    }
+
+    private static class EndState implements ExecutionControl<Long> {
+
+      public boolean executeBlock(@NotNull BaseFlowControl<Long> flowControl) throws Exception {
+        flowControl.stop();
+        return true;
+      }
+
+      public void cancelExecution() {
+      }
+
+      public void abortExecution(@NotNull Throwable error) {
+      }
+    }
+
+    private class MessageCommand implements Runnable {
+
+      private final long message = System.currentTimeMillis();
+
+      public void run() {
+        DelayControl.this.message = message;
+        flowControl.execute();
+      }
+    }
+
+    private class StartState implements ExecutionControl<Long> {
+
+      public boolean executeBlock(@NotNull BaseFlowControl<Long> flowControl) throws Exception {
+        controlState = new MessageState();
+        timer().schedule(DelayControl.this, delayMillis);
+        return false;
+      }
+
+      public void cancelExecution() {
+      }
+
+      public void abortExecution(@NotNull Throwable error) {
+      }
+    }
+
+    private class MessageState implements ExecutionControl<Long> {
+
+      public boolean executeBlock(@NotNull BaseFlowControl<Long> flowControl) throws Exception {
+        final Long message = DelayControl.this.message;
+        if (message != null) {
+          controlState = new EndState();
+          flowControl.postOutput(message);
+          flowControl.stop();
+          return true;
+        }
+        return false;
+      }
+
+      public void cancelExecution() {
+        if (cancel()) {
+          controlState = startState;
+        }
+      }
+
+      public void abortExecution(@NotNull Throwable error) {
+        cancel();
+      }
+    }
+  }
+
+  private static class PeriodControlFactory implements ControlFactory {
+
+    private final long delayMillis;
+    private final long periodMillis;
+
+    private PeriodControlFactory(long delayMillis, long periodMillis) {
+      this.delayMillis = delayMillis;
+      this.periodMillis = periodMillis;
+    }
+
+    @NotNull
+    public ExecutionControl<Long> create(@NotNull Scheduler scheduler) {
+      return new PeriodControl(scheduler, delayMillis, periodMillis);
+    }
+  }
+
+  private static class PeriodControl extends TimerTask implements ExecutionControl<Long> {
+
+    private final ConcurrentLinkedQueue<Long> outputQueue = new ConcurrentLinkedQueue<Long>();
+    private final MessageCommand messageCommand = new MessageCommand();
+    private final StartState startState = new StartState();
+    private final MessageState messageState = new MessageState();
+    private final Scheduler scheduler;
+    private final long delayMillis;
+    private final long periodMillis;
+    private ExecutionControl<Long> controlState = startState;
+    private BaseFlowControl<Long> flowControl;
+
+    private PeriodControl(@NotNull Scheduler scheduler, long delayMillis, long periodMillis) {
+      this.scheduler = scheduler;
+      this.delayMillis = delayMillis;
+      this.periodMillis = periodMillis;
+    }
+
+    public boolean executeBlock(@NotNull BaseFlowControl<Long> flowControl) throws Exception {
+      this.flowControl = flowControl;
+      return controlState.executeBlock(flowControl);
+    }
+
+    public void cancelExecution() throws Exception {
+      controlState.cancelExecution();
+    }
+
+    public void abortExecution(@NotNull Throwable error) throws Exception {
+      controlState.abortExecution(error);
+    }
+
+    public void run() {
+      outputQueue.offer(System.currentTimeMillis());
+      scheduler.scheduleLow(messageCommand);
+    }
+
+    private class MessageCommand implements Runnable {
+
+      public void run() {
+        flowControl.execute();
+      }
+    }
+
+    private class StartState implements ExecutionControl<Long> {
+
+      public boolean executeBlock(@NotNull BaseFlowControl<Long> flowControl) throws Exception {
+        controlState = messageState;
+        timer().scheduleAtFixedRate(PeriodControl.this, delayMillis, periodMillis);
+        return false;
+      }
+
+      public void cancelExecution() {
+      }
+
+      public void abortExecution(@NotNull Throwable error) {
+      }
+    }
+
+    private class MessageState implements ExecutionControl<Long> {
+
+      public boolean executeBlock(@NotNull BaseFlowControl<Long> flowControl) throws Exception {
+        final Long message = outputQueue.poll();
+        if (message != null) {
+          flowControl.postOutput(message);
+          return true;
+        }
+        return false;
+      }
+
+      public void cancelExecution() {
+        if (cancel()) {
+          controlState = startState;
+        }
+      }
+
+      public void abortExecution(@NotNull Throwable error) {
+        cancel();
+      }
+    }
   }
 }
